4b0d5ccf8d0ac690f6660deb3a26367d8b14ebdb
alifalghifari <alifalghifari798@gmail.com>
2020-11-01 14:13:27 +0800

    add img to show/hide password

--- login_react.js
+++ login_react.js
@@ -1,17 +1,55 @@
-function Login() {
-    return (
-        <div className="header">
-            <h2>Login Form</h2>
-            <form className="fill">
-                <label>Name
-                <input type="text" name="nama" value="" />
-                </label>
-                <label>password
-                <input type="password" name="password" value="" />
-                </label>
-            </form>
-        </div>
-    )
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+        this.state = {
+            name: '',
+            password: '',
+            typeP: 'password'
+        };
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleImg = this.handleImg.bind(this);
+    }
+
+
+    handleChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value
+
+        this.setState({ [name]: value })
+    }
+
+    handleImg() {
+        const node = this.myRef.current.type;
+        const value = node === 'password' ? 'text' : 'password';
+
+        this.setState({
+            typeP: value
+        })
+    }
+
+    render() {
+        return (
+            <div className="header">
+                <h2>Login</h2>
+
+                <form onSubmit={this.handleSubmit} className="fill" >
+                    <label>
+                        <input type="text" name="name" value={this.state.name} onChange={this.handleChange} placeholder="Name" autoComplete="off" />
+                        <span>Name</span></label>
+                    <label>
+                        <input ref={this.myRef} type={this.state.typeP} name="password" value={this.state.password} onChange={this.handleChange} placeholder="Password" />
+                        <span>password</span>
+                        <img className="img" src="img/photo.png" alt="" onClick={this.handleImg} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        );
+    }
 }
+
+
 ReactDOM.render(<Login />, document.getElementById('form'))
